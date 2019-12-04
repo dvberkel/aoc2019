f8be6c751d12aa15f908e591d87c01e4ddaf8891
Daan van Berkel <daan.van.berkel@alliander.com>
2019-12-04 08:39:58 +0100

    Solve second problem of day 1

--- day01/solution.py
+++ day01/solution.py
@@ -1,9 +1,21 @@
-def calculate_fuel(modules):
-    fuel = 0
-    for weight in modules:
-        fuel += weight//3 - 2
+def fuel_for(weight):
+    fuel = weight//3 - 2
+    return fuel if fuel > 0 else 0
 
-    return fuel
+def accumulated_fuel_for(weight):
+    accumulated_fuel = 0
+    while weight > 0:
+        fuel = fuel_for(weight)
+        accumulated_fuel += fuel
+        weight = fuel
+    return accumulated_fuel
+
+def calculate_fuel(modules):
+    total_fuel = 0
+    for weight in modules:
+        total_fuel += accumulated_fuel_for(weight)
+
+    return total_fuel
 
 f = open("input.txt", "r")
 
@@ -12,5 +24,5 @@ for line in f:
     weight = int(line.strip())
     modules.append(weight)
 
-fuel = calculate_fuel(modules)
-print(fuel)
+total_fuel = calculate_fuel(modules)
+print(total_fuel)
