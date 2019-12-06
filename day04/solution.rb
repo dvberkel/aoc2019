a77206ee592e9e337b713152cacf8fa5f2b0bb08
Daan van Berkel <daan.van.berkel@alliander.com>
2019-12-06 21:19:38 +0100

    Solve part 2 of day 4

--- day04/solution.rb
+++ day04/solution.rb
@@ -1,15 +1,19 @@
 #! /usr/bin/env ruby
 
-def has_double(digits)
-    for index in (0..digits.length-2) do
-        if digits[index] == digits[index + 1] then
-           return true
-        end
-    end
-    false
+def has_double?(digits)
+    frequencies = frequency(digits)
+    frequencies.has_value?(2)
 end
 
-def decreases(digits)
+def frequency(digits)
+    digits.inject({}) do |hsh, digit|
+        hsh[digit] = 0 if hsh[digit].nil?
+        hsh[digit] = hsh[digit] + 1
+        hsh
+    end
+end
+
+def decreases?(digits)
     for index in (0..digits.length-2) do
         if digits[index] > digits[index + 1] then
            return true
@@ -21,8 +25,8 @@ end
 count = 0
 for n in (359282..820401) do
     digits = n.digits.reverse()
-    if !has_double(digits) then next end
-    if decreases(digits) then next end
+    if decreases?(digits) then next end
+    if !has_double?(digits) then next end
     count += 1
 end
 
