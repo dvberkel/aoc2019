29c3756d84929d2cdbab96699d92f2fb86403c0a
Daan van Berkel <daan.van.berkel@alliander.com>
2019-12-10 07:53:31 +0100

    Solve part 1 of day 5

--- day05/solution/src/main/java/nl/fifthpostulate/advent/Application.java
+++ day05/solution/src/main/java/nl/fifthpostulate/advent/Application.java
@@ -10,21 +10,8 @@ public class Application {
         String input = readInputFromFile("input.txt");
         int[] source = toProgram(input.trim());
 
-        outer: for (int noun = 0; noun < 100; noun++) {
-            for (int verb = 0; verb < 100; verb++) {
-                int[] program = Arrays.copyOf(source, source.length);
-                program[1] = noun;
-                program[2] = verb;
-
-                IntProgram computer = new IntProgram(program);
-                computer.run();
-
-                if (program[0] == 19690720) {
-                    System.out.printf("%d", 100 * noun + verb);
-                    break outer;
-                }
-            }
-        }
+        IntProgram computer = new IntProgram(source);
+        computer.run();
     }
 
     private static String readInputFromFile(String filePath) throws IOException {
--- day05/solution/src/main/java/nl/fifthpostulate/advent/IntProgram.java
+++ day05/solution/src/main/java/nl/fifthpostulate/advent/IntProgram.java
@@ -1,5 +1,7 @@
 package nl.fifthpostulate.advent;
 
+import javax.xml.xpath.XPathEvaluationResult;
+
 public class IntProgram {
     private final int[] memory;
     private int instructionPointer = 0;
@@ -17,8 +19,10 @@ public class IntProgram {
     }
 
     public Opcode step() {
-        Opcode opcode = Opcode.from(memory[instructionPointer]);
-        opcode.execute(instructionPointer, memory);
+        int description = memory[instructionPointer];
+        Opcode opcode = Opcode.from(description % 100);
+        Parameters parameters = Parameters.from(description / 100);
+        opcode.execute(instructionPointer, memory, parameters);
         instructionPointer += opcode.increment();
         return opcode;
     }
@@ -26,62 +30,134 @@ public class IntProgram {
     public int[] dump() {
         return memory;
     }
+}
 
-    private enum Opcode {
-        Add {
-            @Override
-            public void execute(int instructionPointer, int[] memory) {
-                int leftIndex = memory[instructionPointer + 1];
-                int left = memory[leftIndex];
-                int rightIndex = memory[instructionPointer + 2];
-                int right = memory[rightIndex];
-                int index = memory[instructionPointer + 3];
-                memory[index] = left + right;
-            }
-        },
-        Multiply {
-            @Override
-            public void execute(int instructionPointer, int[] memory) {
-                int leftIndex = memory[instructionPointer + 1];
-                int left = memory[leftIndex];
-                int rightIndex = memory[instructionPointer + 2];
-                int right = memory[rightIndex];
-                int index = memory[instructionPointer + 3];
-                memory[index] = left * right;
-            }
-        },
-        Stop {
-            @Override
-            public void execute(int instructionPointer, int[] memory) {
-                // do nothing
-            }
-
-            @Override
-            public int increment() {
-                return 1;
-            }
-        },
-        Unknown {
-            @Override
-            public void execute(int instructionPointer, int[] memory) {
-                throw new IllegalArgumentException(String.format("unknown opcode '%d' at index %d", memory[instructionPointer], instructionPointer));
-            }
-        };
-
-        public static Opcode from(int description) {
-            switch (description) {
-                case 1: return Add;
-                case 2: return Multiply;
-                case 99: return Stop;
-                default: return Unknown;
-            }
+enum Opcode {
+    Add {
+        @Override
+        public void execute(int instructionPointer, int[] memory, Parameters parameters) {
+            int left = parameters.first.value(instructionPointer + 1, memory);
+            int right = parameters.second.value(instructionPointer + 2, memory);
+            int index = Parameter.Immediate.value(instructionPointer + 3, memory);
+            memory[index] = left + right;
+        }
+    },
+    Multiply {
+        @Override
+        public void execute(int instructionPointer, int[] memory, Parameters parameters) {
+            int left = parameters.first.value(instructionPointer + 1, memory);
+            int right = parameters.second.value(instructionPointer + 2, memory);
+            int index = Parameter.Immediate.value(instructionPointer + 3, memory);
+            memory[index] = left * right;
+        }
+    },
+    Stop {
+        @Override
+        public void execute(int instructionPointer, int[] memory, Parameters parameters) {
+            // do nothing
         }
 
-        public abstract void execute(int instructionPointer, int[] memory);
-
+        @Override
         public int increment() {
-            return 4;
+            return 1;
+        }
+    },
+    Input {
+        @Override
+        public void execute(int instructionPointer, int[] memory, Parameters parameters) {
+            Parameter parameter = Parameter.Immediate;
+            int index = parameter.value(instructionPointer + 1, memory);
+            memory[index] = 1; // After providing 1 to the only input instruction
+        }
+
+        @Override
+        public int increment() { return 2; }
+    },
+    Output {
+        @Override
+        public void execute(int instructionPointer, int[] memory, Parameters parameters) {
+            int value = parameters.first.value(instructionPointer + 1, memory);
+            System.out.format("%d\n", value);
+        }
+
+        @Override
+        public int increment() { return 2; }
+    },
+    Unknown {
+        @Override
+        public void execute(int instructionPointer, int[] memory, Parameters parameters) {
+            throw new IllegalArgumentException(String.format("unknown opcode '%d' at index %d", memory[instructionPointer], instructionPointer));
+        }
+    };
+
+    public static Opcode from(int description) {
+        switch (description) {
+            case 1: return Add;
+            case 2: return Multiply;
+            case 3: return Input;
+            case 4: return Output;
+            case 99: return Stop;
+            default: return Unknown;
         }
     }
+
+    public abstract void execute(int instructionPointer, int[] memory, Parameters parameters);
+
+    public int increment() {
+        return 4;
+    }
+}
+
+class Parameters {
+    public static Parameters from(int description) {
+        Parameter first = Parameter.from(description % 10);
+        description /= 10;
+        Parameter second = Parameter.from(description % 10);
+        description /= 10;
+        Parameter third = Parameter.from(description % 10);
+        return new Parameters(first, second, third);
+
+    }
+    public final Parameter first;
+    public final Parameter second;
+    public final Parameter third;
+
+    private Parameters(Parameter first, Parameter second, Parameter third) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+}
+
+enum Parameter {
+    Position {
+        @Override
+        public int value(int pointer, int[] memory) {
+            int index = memory[pointer];
+            return memory[index];
+        }
+    },
+    Immediate {
+        @Override
+        public int value(int pointer, int[] memory) {
+            return memory[pointer];
+        }
+    },
+    Unknown {
+        @Override
+        public int value(int pointer, int[] memory) {
+            throw new IllegalArgumentException(String.format("unknown parameter '%d' at index %d", memory[pointer], pointer));
+        }
+    };
+
+    public static Parameter from(int code) {
+        switch (code) {
+            case 0: return Position;
+            case 1: return Immediate;
+            default: return Unknown;
+        }
+    }
+
+    public abstract int value(int pointer, int[] memory);
 }
 
